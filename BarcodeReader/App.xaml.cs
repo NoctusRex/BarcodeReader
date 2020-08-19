e767ac9337b2f5819a2b1ab2a86a1ddcf46f73a2
Noctus Rex <noxjudex@gmail.com>
2020-08-19 11:05:34 +0200

    Added already running detection

--- BarcodeReader/App.xaml.cs
+++ BarcodeReader/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 using System.Windows.Threading;
 
 namespace BarcodeReader
@@ -8,11 +10,40 @@ namespace BarcodeReader
     /// </summary>
     public partial class App : Application
     {
+        private Mutex mutex;
+        private bool handleExceptions = false;
+
         private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
-            e.Handled = true;
+            e.Handled = handleExceptions;
 
             MessageBox.Show(e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            CheckAlreadyRunning();
+
+            base.OnStartup(e);
+        }
+
+        private void CheckAlreadyRunning()
+        {
+            mutex = new Mutex(false, "Barcode Reader - 133742069");
+
+            if (!mutex.WaitOne(0, false))
+                throw new Exception("The application is already running.");
+            
+            handleExceptions = true;
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            mutex?.Close();
+            mutex?.Dispose();
+            mutex = null;
+
+            base.OnExit(e);
         }
 
     }
